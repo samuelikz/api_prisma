6f6faf707a164f9a53bed52ff8a1453cb3ebe533
samuelikz <samuel.nds@outlook.com>
2023-04-20 16:45:22 -0300

    local to remote

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import routes from '../src/routes/routes';
 import errorRoutes from './routes/errorRoutes';
+import cors from 'cors';
 import path from 'path';
 
 const app = express();
+
+app.use(cors({
+    origin: ['dominio.com.br','local.com.br'],
+    methods: ['GET','POST']
+}));
 
 app.use(express.json());
 app.set('view engine', 'ejs');
