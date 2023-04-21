bf69eed36c97d301614f59fd8ec1792634f1b0bf
samuel-n-silva <samuel_nunes-silva@estudante.sesisenai.org.br>
2023-04-21 17:21:10 -0300

    update

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -18,6 +18,7 @@ export const createUser = async (req: Request, res: Response) => {
 }
 
 // 20/04/2023 Created function on created post from form samuelikz;
+
 export const createPost = async (req: Request, res: Response) => {
     try {
         const { nome, email, cpf, data_nascimento, description } = req.body;
@@ -26,29 +27,52 @@ export const createPost = async (req: Request, res: Response) => {
         const hashedEMAIL = await Crypto.hashd(email);
 
         const post = await prisma.post.create({
-            data: { nome, email: hashedEMAIL, cpf: hashedCPF, data_nascimento, description },
+            data: {
+                nome,
+                email: hashedEMAIL,
+                cpf: hashedCPF,
+                data_nascimento,
+                description
+            }
         });
 
         res.json(post);
     } catch (error) {
+
         console.error(error);
         res.status(500).send('Erro ao cadastrar o registro');
+
     }
 };
 
 // 20/04/2023 Created function on get users informations samuelikz;
+
 export const getUsers = async (req: Request, res: Response) => {
     try {
+
         const users = await prisma.user.findMany();
         res.json(users);
+
     } catch (error) {
+
         console.error(error);
         res.status(500).send('Erro ao buscar os registros');
+
     }
 }
 
 // 20/04/2023 Created function on get post informations samuelikz;
+
 export const getPosts = async (req: Request, res: Response) => {
-    const posts = await prisma.post.findMany();
-    res.json(posts);
+    try {
+
+        const posts = await prisma.post.findMany();
+        res.json(posts);
+
+    } catch (error) {
+
+        console.error(error);
+        res.status(500).send('Erro ao buscar os registros');
+
+    }
 }
