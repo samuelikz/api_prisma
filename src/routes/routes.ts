85313bb6d6e6cb6986896567d606239529c1c905
samuel-n-silva <samuel_nunes-silva@estudante.sesisenai.org.br>
2023-04-21 23:19:49 -0300

    Bug Fixed
    Bug Fixed

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { createLogging, createRegistration, getLogging, getRegistration, createUser} from '../controllers/UserController';
+import { createLogging, createRegistration, getLogging, getRegistration, createUser} from '../controllers/userController';
 
 const router = Router();
 
